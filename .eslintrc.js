e9d59db71ac458932253d49847b4a0a89e9f9a46
Dima Levchenko <112283201+levandowskiy@users.noreply.github.com>
2024-06-03 15:56:55 +0300

    start project

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "react"
+    ],
+    "rules": {
+        'react/react-in-jsx-scope': 'off',
+        'no-var': 'error',
+        'react/prop-types': 'off',
+        'prefer-const': 'warn',
+        'prettier/prettier': [
+          'warn',
+          {
+            "endOfLine": 'auto',
+          },
+        ],
+      },
+}
